c17f46b805f87c6550e441e5caacf3eeb5d26f45
Saikumar Bandigi <bandigisaikumar@gmail.com>
2024-01-11 18:29:51 +0530

    added project details in main file

--- src/main/java/web/GoogleProject/GoogleProjectApplication.java
+++ src/main/java/web/GoogleProject/GoogleProjectApplication.java
@@ -3,6 +3,8 @@ package web.GoogleProject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// crud between this and react-springboot-crud(vs code)
 @SpringBootApplication
 public class GoogleProjectApplication {
 
